ca957f581572e20c2ee8b31a42a93661f72721ac
Ramon Osuna <ramonosuna72@gmail.com>
2021-09-20 11:16:28 -0600

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samuari X', 'Dragon Ball'];
+
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>
+
+            <AddCategory setCategories = { setCategories } />
+
+            <hr />
+            
+            <ol>
+                { 
+                    categories.map( category => 
+                        <GifGrid 
+                            key = { category }
+                            category = { category } 
+                        />
+                    )
+                }
+            </ol>
+
+        </>
+    );
+
+}
+
+export default GifExpertApp
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GifExpertApp from './GifExpertApp';
+
+ReactDOM.render(
+    <GifExpertApp />,
+  document.getElementById('root')
+);
